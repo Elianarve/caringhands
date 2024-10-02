ba3f36bedb3a701b605b4a8706dbe0e2188ef94f
juanvprada <juanvprada@gmail.com>
2024-10-02 14:08:06 +0200

    feat: add Benefits page

--- client/src/router/router.jsx
+++ client/src/router/router.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
 
-const router = () => {
-  return (
-    <div>router</div>
-  )
-}
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../pages/Home';
+import Benefits from '../pages/benefits/Benefits';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/benefits',
+    element: <Benefits />,
+  },
+]);
 
 export default router;
